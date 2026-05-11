7fcb0e2689d4f26ea9964c6226e5d78e4ceb2581
Kajal1395 <60035417+Kajal1395@users.noreply.github.com>
2026-05-11 08:41:52 +0530

    Add: two-integer-sum-ii - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-1.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} numbers
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(numbers, target) {
+        let seen=new Map()
+        for(let i=0;i<numbers.length;i++){
+            let diff=target-numbers[i]
+            if(seen.has(diff) && diff < numbers[i]){
+                return [seen.get(diff)+1,i+1]
+            }
+            else{
+                seen.set(numbers[i],i)
+            }
+        }
+    }
+}
