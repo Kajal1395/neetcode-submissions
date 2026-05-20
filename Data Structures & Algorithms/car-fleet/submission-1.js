a98b5f2b84ae4b026571dd1984c895383f7c39c1
Kajal1395 <60035417+Kajal1395@users.noreply.github.com>
2026-05-20 23:52:52 +0530

    Add: car-fleet - submission-1

---
+++ Data Structures & Algorithms/car-fleet/submission-1.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {number} target
+     * @param {number[]} position
+     * @param {number[]} speed
+     * @return {number}
+     */
+    carFleet(target, position, speed) {
+        let carTimearr=[]
+        let count=0
+        for(let i=0;i<position.length;i++){
+            const time=(target-position[i])/speed[i]
+            carTimearr.push([position[i],time])
+        }
+        carTimearr.sort((a,b)=>b[0]-a[0])
+        console.log(carTimearr,'2darr')
+        let prevTime=0
+        for(let i=0;i<carTimearr.length;i++){
+           if(carTimearr[i][1]>prevTime){
+            count++
+            prevTime=carTimearr[i][1]
+           }
+        }
+        return count
+    }
+}
