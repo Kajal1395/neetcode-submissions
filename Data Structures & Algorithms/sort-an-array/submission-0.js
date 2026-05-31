684b8c77d6106b336d4c1a5b710055d8e0f9ef32
Kajal1395 <60035417+Kajal1395@users.noreply.github.com>
2026-05-31 14:03:23 +0530

    Add: sort-an-array - submission-0

---
+++ Data Structures & Algorithms/sort-an-array/submission-0.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    sortArray(nums) {
+        let n = nums.length;
+
+        for (let i = 0; i < n - 1; i++) {
+            for (let j = 0; j < n - 1 - i; j++) {
+                if (nums[j] > nums[j + 1]) {
+                    [nums[j], nums[j + 1]] = [nums[j + 1], nums[j]];
+                }
+            }
+        }
+
+        return nums;
+    }
+}
