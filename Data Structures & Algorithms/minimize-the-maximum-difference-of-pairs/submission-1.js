e3f6f6b7890632675fb344b7fc782daaaf112d49
Kajal1395 <60035417+Kajal1395@users.noreply.github.com>
2026-07-02 21:23:06 +0530

    Add: minimize-the-maximum-difference-of-pairs - submission-1

---
+++ Data Structures & Algorithms/minimize-the-maximum-difference-of-pairs/submission-1.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} p
+     * @return {number}
+     */
+    minimizeMax(nums, p) {
+        let low = 0;
+        let n = nums.length;
+        let high = Math.max(...nums) - Math.min(...nums);
+        let ans = 0;
+        nums.sort((a, b) => a - b);
+        while (low <= high) {
+            let mid = Math.floor((low + high) / 2);
+            let pairFound = 0;
+            for (let i = 0; i < n; i++) {
+                if (Math.abs(nums[i] - nums[i + 1]) <= mid) {
+                    pairFound++;
+                    i++
+                }
+            }
+            if (pairFound >= p) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+}
