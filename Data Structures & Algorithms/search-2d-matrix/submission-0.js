f21a55b80063bfb856443dec3e3a8264c4a8646e
Kajal1395 <60035417+Kajal1395@users.noreply.github.com>
2026-07-02 12:24:16 +0530

    Add: search-2d-matrix - submission-0

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-0.js
@@ -0,0 +1,39 @@
+class Solution {
+    /**
+     * @param {number[][]} matrix
+     * @param {number} target
+     * @return {boolean}
+     */
+    searchMatrix(matrix, target) {
+        let m = matrix.length;
+        let n = matrix[0].length;
+        let low = 0;
+        let high = m * n - 1;
+        while (low <= high) {
+            let mid = Math.floor((low + high) / 2);
+            let row = Math.floor(mid / n);
+            let col = Math.floor(mid % n);
+            let value = matrix[row][col];
+            if (value === target) return true;
+            else if (value < target) low = mid + 1;
+            else high = mid - 1;
+        }
+        return false;
+        TC: O(n * m);
+        // let res = [];
+        // for (let mat of matrix) {
+        //     for (let ele of mat) {
+        //         res.push(ele);
+        //     }
+        // }
+        // let low = 0;
+        // let high = res.length-1;
+        // while (low <= high) {
+        //     let mid = Math.floor((low + high) / 2);
+        //     if (res[mid] === target) return true;
+        //     else if (res[mid] < target) low = mid + 1;
+        //     else high = mid - 1;
+        // }
+        // return false;
+    }
+}
