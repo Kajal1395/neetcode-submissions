29d304dcdc6dea0d53bef55c566369b41c433da6
Kajal1395 <60035417+Kajal1395@users.noreply.github.com>
2026-07-09 16:26:33 +0530

    Add: find-in-mountain-array - submission-1

---
+++ Data Structures & Algorithms/find-in-mountain-array/submission-1.js
@@ -0,0 +1,65 @@
+/**
+ * // This is the MountainArray's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * class MountainArray {
+ *     @param {number} index
+ *     @return {number}
+ *     get(index) {
+ *         ...
+ *     }
+ *
+ *     @return {number}
+ *     length() {
+ *         ...
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {number} target
+     * @param {MountainArray} mountainArr
+     * @return {number}
+     */
+    findInMountainArray(target, mountainArr) {
+        //find peak
+        let low = 0;
+        let n = mountainArr.length();
+        let high = n - 1;
+        while (low < high) {
+            let mid = Math.floor((low + high) / 2);
+            if (mountainArr.get(mid) < mountainArr.get(mid + 1)) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+
+        if (mountainArr.get(high) === target) return high;
+
+        let left = 0;
+        let right = high - 1;
+        while (left <= right) {
+            let mid = Math.floor((left + right) / 2);
+            if (mountainArr.get(mid) === target) return mid;
+            if (mountainArr.get(mid) > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        let left1 = high + 1;
+        let right1 = n - 1;
+        while (left1 <= right1) {
+            let mid = Math.floor((left1 + right1) / 2);
+            if (mountainArr.get(mid) === target) return mid;
+            if (mountainArr.get(mid) < target) {
+                right1 = mid - 1;
+            } else {
+                left1 = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
