a6054dca6edd7a34bc94cd78dc9c044fc55465e0
Kajal Pandey <60035417+Kajal1395@users.noreply.github.com>
2026-09-08 19:47:59 +0530

    Add: house-robber-iii - submission-1

---
+++ Data Structures & Algorithms/house-robber-iii/submission-1.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {number}
+     */
+    rob(root) {
+        function dfs(node) {
+            if (!node) return [0, 0];
+            let left = dfs(node.left);
+            let right = dfs(node.right);
+            return [node.val + left[1] + right[1], Math.max(...left) + Math.max(...right)];
+        }
+        let res = dfs(root);
+        return Math.max(...res);
+    }
+}
