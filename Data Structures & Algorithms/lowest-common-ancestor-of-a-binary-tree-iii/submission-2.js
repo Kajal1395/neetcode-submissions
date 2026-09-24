e2f0326024f33d9ede6398f9661eb164374db024
Kajal Pandey <60035417+Kajal1395@users.noreply.github.com>
2026-09-24 19:17:07 +0530

    Add: lowest-common-ancestor-of-a-binary-tree-iii - submission-2

---
+++ Data Structures & Algorithms/lowest-common-ancestor-of-a-binary-tree-iii/submission-2.js
@@ -0,0 +1,25 @@
+/**
+ * // Definition for a Node.
+ * function Node(val) {
+ *    this.val = val;
+ *    this.left = null;
+ *    this.right = null;
+ *    this.parent = null;
+ * }
+ */
+class Solution {
+    /**
+     * @param {Node} p
+     * @param {Node} q
+     * @return {Node}
+     */
+    lowestCommonAncestor(p, q) {
+        let a = p;
+        let b = q;
+        while (a !== b) {
+            a = a ? a.parent : q;
+            b = b ? b.parent : p;
+        }
+        return a;
+    }
+}
